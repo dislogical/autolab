98b66d6059a06605c92d3b4743c1b3e05eb8ecf6
Colden Cullen <colden@coldencullen.com>
2025-07-31 18:40:17 +0000

    ci: migrate github actions to devenv

--- devenv.nix
+++ devenv.nix
@@ -27,4 +27,29 @@
       pyyaml
     ]);
   };
+
+  tasks = {
+    "devenv:enterTest".exec = "tilt ci";
+  };
+
+  scripts = {
+    publish-manifest.exec = ''
+      OCI_REPO=oci://ghcr.io/dislogical/autolab/manifest
+      IMAGE=$OCI_REPO:$(git rev-parse --short HEAD)
+
+      # Render the manifests
+      task render:prod
+
+      # Push the manifest to the repo
+      flux push artifact $IMAGE -f deploy/prod/kustomized.yaml \
+        --reproducible \
+        --source=$(git config --get remote.origin.url) \
+        --revision=$(git branch --show-current)@sha1:$(git rev-parse HEAD) \
+        --annotations='org.opencontainers.image.licenses=MIT' \
+        --annotations='org.opencontainers.image.source=https://github.com/dislogical/autolab'
+
+      # Tag the manifest as latest
+      flux tag artifact $IMAGE --tag latest
+    '';
+  };
 }
